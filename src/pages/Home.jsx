5a5386cac360a562e902fbd56a2915fe6a58cfcf
Mostapha Yasser <mostaphayasser18@yahoo.com>
2024-10-15 13:45:12 +0300

    fix: dashbord Responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,33 @@
 import { useState } from "react";
 import Sidebar from "../Layouts/Sidebar";
 import { Outlet } from "react-router-dom";
-import { Ellipsis } from "lucide-react";
+import { Ellipsis, Phone } from "lucide-react";
 
 function Home() {
   const [open, setOpen] = useState(window.innerWidth >= 768);
 
   return (
     <section>
-      <div className="sticky top-0 bg-light-dark h-6 px-2 text-text hidden md:block z-10 ">
-      <Ellipsis
+      <div className="sticky top-0   bg-light-dark h-6 px-2 text-text hidden md:flex justify-between z-10 ">
+        <Ellipsis
           size={30}
           className="cursor-pointer"
           onClick={() => setOpen(!open)}
         />
+        <div className="text-main font-bold w-64 pr-5  mr-10 flex justify-between ">
+          <a href="tel:17002" className="flex justify-around w-20 align-baseline">
+            <Phone size={20} className="pt-1"/> 17002
+          </a>
+          <a href="mailto:ITIinfo@iti.gov.eg"> ITIinfo@iti.gov.eg</a>
+        </div>
       </div>
       <div className="flex">
-      <aside className={`  sticky  top-0 md:top-6  h-screen transition-all duration-300 'w-fit'`}>
-      <Sidebar open={open} setOpen={setOpen} />
+        <aside
+          className={`  sticky  top-0 md:top-6  h-screen transition-all duration-300 'w-fit'`}
+        >
+          <Sidebar open={open} setOpen={setOpen} />
         </aside>
-        <div className="flex-grow px-16 ">
+        <div className="flex-grow md:px-12 lg:px-14">
           <Outlet />
         </div>
       </div>
